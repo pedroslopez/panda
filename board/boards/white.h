a7b074f632c0089a9044ca3bd295dc3c8e0c7235
Pedro Lopez <pedroslopez@me.com>
2020-12-09 03:52:16 -0400

    disable CAN3

--- board/boards/white.h
+++ board/boards/white.h
@@ -11,7 +11,7 @@ void white_enable_can_transceiver(uint8_t transceiver, bool enabled) {
       set_gpio_output(GPIOC, 13, !enabled);
       break;
     case 3U:
-      set_gpio_output(GPIOA, 0, !enabled);
+      set_gpio_output(GPIOA, 0, false);
       break;
     default:
       puts("Invalid CAN transceiver ("); puth(transceiver); puts("): enabling failed\n");
